48101c3fd788d52d771ac454aa9298ae97e090a2
ZXW2600 <zhaoxinwei74@gmail.com>
2021-07-06 21:39:27 +0800

    完成bounding box 选择和VOC数据录入

---
+++ VOC.py
@@ -0,0 +1,74 @@
+#! python3
+# -*- encoding: utf-8 -*-
+'''
+@File   		:   VOC.py
+@Time    		:   2021/07/06 21:21:42
+@Author  		:   ZXW2600 
+@Version 		:   1.0
+@Contact 		:   zhaoxiwnei74@gmail.com
+@description	:   生成VOC标注
+'''
+
+from lxml import etree
+ 
+class GEN_Annotations:
+    def __init__(self, filename):
+        self.root = etree.Element("annotation")
+ 
+        child1 = etree.SubElement(self.root, "folder")
+        child1.text = "VOC2007"
+ 
+        child2 = etree.SubElement(self.root, "filename")
+        child2.text = filename
+ 
+        child3 = etree.SubElement(self.root, "source")
+ 
+        child4 = etree.SubElement(child3, "annotation")
+        child4.text = "PASCAL VOC2007"
+        child5 = etree.SubElement(child3, "database")
+        child5.text = "Unknown"
+ 
+        child6 = etree.SubElement(child3, "image")
+        child6.text = "flickr"
+        child7 = etree.SubElement(child3, "flickrid")
+        child7.text = "35435"
+ 
+ 
+    def set_size(self,witdh,height,channel):
+        size = etree.SubElement(self.root, "size")
+        widthn = etree.SubElement(size, "width")
+        widthn.text = str(witdh)
+        heightn = etree.SubElement(size, "height")
+        heightn.text = str(height)
+        channeln = etree.SubElement(size, "depth")
+        channeln.text = str(channel)
+    def savefile(self,filename):
+        tree = etree.ElementTree(self.root)
+        tree.write(filename, pretty_print=True, xml_declaration=False, encoding='utf-8')
+    def add_pic_attr(self,label,xmin,ymin,width,height):
+        object = etree.SubElement(self.root, "object")
+        namen = etree.SubElement(object, "name")
+        namen.text = label
+        bndbox = etree.SubElement(object, "bndbox")
+        xminn = etree.SubElement(bndbox, "xmin")
+        xminn.text = str(xmin)
+        yminn = etree.SubElement(bndbox, "ymin")
+        yminn.text = str(ymin)
+        xmaxn = etree.SubElement(bndbox, "xmax")
+        xmaxn.text = str(xmin+width)
+        ymaxn = etree.SubElement(bndbox, "ymax")
+        ymaxn.text = str(ymin+height)
+ 
+ 
+if __name__ == '__main__':
+    filename="000001.jpg"
+    anno= GEN_Annotations(filename)
+    anno.set_size(1280,720,3)
+    for i in range(3):
+        xmin=i+1
+        ymin=i+10
+        xmax=i+100
+        ymax=i+100
+        anno.add_pic_attr("mouse",xmin,ymin,xmax,ymax)
+    anno.savefile("00001.xml")
+
--- camera.py
+++ camera.py
@@ -1,7 +1,7 @@
 #! python3
 # -*- encoding: utf-8 -*-
 '''
-@File   		:   autoTag.py
+@File   		:   camera.py
 @Time    		:   2021/07/06 18:23:30
 @Author  		:   ZXW2600
 @Version 		:   1.0
@@ -11,41 +11,74 @@
 import cv2
 import numpy as np
 
-camera = cv2.VideoCapture(2)
-while True:
-    _, pic = camera.read()
-    pic_gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
-    # pic_threshold=cv2.adaptiveThreshold(pic_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,adaptiveThreshold_block,adaptiveThreshold_C)
-    ret, pic_threshold = cv2.threshold(pic_gray, 125, 255, cv2.THRESH_OTSU)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
-    pic_open = cv2.morphologyEx(pic_threshold, cv2.MORPH_OPEN, kernel)
-    pic_open = 255-pic_open
 
-    sp = pic.shape
-    height = sp[0]  # height(rows) of image
-    width = sp[1]  # width(colums) of image
-    points = [(0, 0), (width, 0), (0, height), (width, height)]
+def pointInRectangle(point, rectangle):
+    x, y, w, h = rectangle
+    if (point[0] > x and point[0] < w+x) and (point[1] > y or point[1] < h+y):
+        return 1
+    elif (point[0] < x or point[0] < w+x) or (point[1] < y or point[1] > h+y):
+        return -1
+    else:
+        return 0
 
-    contours, hier = cv2.findContours(
-        pic_open, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours_value = []
-    area=[]
-    for cidx, cnt in enumerate(contours):
-        if cv2.pointPolygonTest(cnt, points[0], False) == -1 and\
-                cv2.pointPolygonTest(cnt, points[1], False) == -1 and\
-                cv2.pointPolygonTest(cnt, points[2], False) == -1 and\
-                cv2.pointPolygonTest(cnt, points[3], False) == -1:
-            contours_value.append(cnt)
-            area.append(cv2.contourArea(cnt))
 
-    if len(area):
-        max_idx = np.argmax(area)
-        max_countour=contours_value[max_idx]
-        max_rectangle=cv2.boundingRect(max_countour)
-        cv2.rectangle(pic,max_rectangle,(255,0,0),2)
-    
-    cv2.imshow("demo", pic)
-    cv2.imshow("threshold", pic_threshold)
-    cv2.imshow("open", pic_open)
+class Camera:
+    def __init__(self, camera_index):
+        self.camera = cv2.VideoCapture(2)
 
-    cv2.waitKey(1)
+    def takeAndWritePicture(self, filename, object_tag):
+        """[拍照并检测物体位置]
+        Args:
+            filename ([type]): [保存的文件名]
+            object_tag ([type]): [标签]
+        """
+        _, pic = self.camera.read()
+        pic_gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+        # pic_threshold=cv2.adaptiveThreshold(pic_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,adaptiveThreshold_block,adaptiveThreshold_C)
+        ret, pic_threshold = cv2.threshold(pic_gray, 125, 255, cv2.THRESH_OTSU)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
+        pic_open = cv2.morphologyEx(pic_threshold, cv2.MORPH_OPEN, kernel)
+        pic_open = 255-pic_open
+
+        sp = pic.shape
+        height = sp[0]  # height(rows) of image
+        width = sp[1]  # width(colums) of image
+        points = [(0, 0), (width, 0), (0, height), (width, height)]
+
+        contours, hier = cv2.findContours(
+            pic_open, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours_value = []
+        area = []
+        for cidx, cnt in enumerate(contours):
+            if pointInRectangle(points[0], cv2.boundingRect(cnt)) == -1 and\
+                    pointInRectangle(points[1], cv2.boundingRect(cnt)) == -1 and\
+                    pointInRectangle(points[2], cv2.boundingRect(cnt)) == -1 and\
+                    pointInRectangle(points[3], cv2.boundingRect(cnt)) == -1:
+                contours_value.append(cnt)
+                area.append(cv2.contourArea(cnt))
+
+        pic_tag = np.copy(pic)
+        x, y, w, h = 0, 0, 0, 0
+        if len(area):
+            max_idx = np.argmax(area)
+            max_countour = contours_value[max_idx]
+            max_rectangle = cv2.boundingRect(max_countour)
+            cv2.rectangle(pic_tag, max_rectangle, (255, 0, 0), 2)
+            x, y, w, h = max_rectangle
+            cv2.putText(pic_tag, object_tag, (x, y),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+            ret = True
+            cv2.imwrite(filename, pic)
+        else:
+            ret = False
+
+        return ret, pic_tag, x, y, w, h
+
+
+if __name__ == "__main__":
+    demo = Camera(2)
+    while True:
+        ret, pic, x_1, y_1, w_1, h_1 = demo.takeAndWritePicture(
+            "000.png", "demo")
+        cv2.imshow("test", pic)
+        cv2.waitKey(1)
